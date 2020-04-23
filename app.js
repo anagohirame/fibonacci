9856ea789c35e385658bdb96d8b703f1ee4a0a54
Wataru Ochi <wwwataruochi@live.jp>
2020-04-23 18:20:17 +0900

    メモ化再帰Fibonacciの実装

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+const dp = new Map();
+dp.set(0, 0);
+dp.set(1, 1);
+function fib(n) {
+	if (dp.has(n)) {
+		return dp.get(n);
+	}
+	const value = fib(n-1) + fib(n-2);
+	dp.set(n, value);
+	return value;
+}
+const length = 40;
+for (let i = 0; i <= length; ++i) console.log(fib(i));
